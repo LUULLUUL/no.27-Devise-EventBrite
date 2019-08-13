a3e26e24d72069dd5ba0f84b411c5563fca8a1e3
LU Jiaying <lujiaying7@gmail.com>
2019-08-13 23:57:28 +0200

    use devise gem, creating models and controllers

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,16 @@
+class EventsController < ApplicationController
+  def show
+  end
+
+  def new
+  end
+
+  def index
+  end
+
+  def edit
+  end
+
+  def _form
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'events/show'
+  get 'events/new'
+  get 'events/index'
+  get 'events/edit'
+  get 'events/_form'
+  get 'users/show'
   root to: 'pages#home'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html\
